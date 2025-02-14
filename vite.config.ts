1c41018662bcb3b408c1a07cddec7e11d994f6b6
jackson36067 <jacksonn36067@gmail.com>
2025-02-14 17:16:55 +0800

    移除tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni(), tailwindcss()],
+  plugins: [uni()],
   build: {
     rollupOptions: {
       external: ["destr", "deep-pick-omit"],
